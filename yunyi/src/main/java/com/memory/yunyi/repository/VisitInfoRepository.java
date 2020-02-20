1ad23e334dd520b4ea9ad16460be4b3da50d6f1d
chaseMengdi <1018952520@qq.com>
2020-02-20 16:50:01 +0800

    修改User实体类后，更新DAO层，注意jpa驼峰与下划线以及nativeQuery问题

--- yunyi/src/main/java/com/memory/yunyi/repository/VisitInfoRepository.java
+++ yunyi/src/main/java/com/memory/yunyi/repository/VisitInfoRepository.java
@@ -1,4 +1,5 @@
 package com.memory.yunyi.repository;
+
 import com.memory.yunyi.entity.VisitInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -6,8 +7,22 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 
-
-public interface VisitInfoRepository extends JpaRepository<VisitInfo,Integer>{
-    @Query("select a from VisitInfo a where a.user.hometown = ?1 order by a.likeNum  DESC ")
+public interface VisitInfoRepository extends JpaRepository<VisitInfo, Integer> {
+    /**
+     * 获取同一家乡的用户
+     *
+     * @param hometown
+     * @return
+     */
+    @Query("select a from VisitInfo a where a.user.city = ?1 order by a.likeNum  DESC ")
     List<VisitInfo> listByHometown(String hometown);
+
+    /**
+     * 根据openId获取主页访问信息
+     *
+     * @param openId
+     * @return
+     */
+    @Query(value = "SELECT * FROM visitInfo WHERE userid=?1", nativeQuery = true)
+    VisitInfo findByOpenId(String openId);
 }
--- yunyi/src/main/java/com/memory/yunyi/repository/processRepository.java
+++ yunyi/src/main/java/com/memory/yunyi/repository/processRepository.java
@@ -1,15 +1,16 @@
 package com.memory.yunyi.repository;
+
+import com.memory.yunyi.entity.process;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import com.memory.yunyi.entity.process;
 
 import javax.transaction.Transactional;
 
 
-public interface processRepository extends JpaRepository<process,Integer> {
+public interface processRepository extends JpaRepository<process, Integer> {
     @Query(value = "insert into process(id,adminid,reason_num,ttime,userid) value(?1,?2,?3,?4,?5)", nativeQuery = true)
     @Modifying
     @Transactional
-    void reup(Integer id,Integer adminID, String Num, String ttime, Integer userid);
+    void reup(Integer id, Integer adminID, String Num, String ttime, String userid);
 }
