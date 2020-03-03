5246ecec8864347cea2be21103543ac902bbeb76
chaseMengdi <1018952520@qq.com>
2020-03-03 14:39:07 +0800

    评论显示问题修复

--- yunyi/src/main/java/com/memory/yunyi/repository/CommentRepository.java
+++ yunyi/src/main/java/com/memory/yunyi/repository/CommentRepository.java
@@ -10,6 +10,10 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
     //    查询某一用户收到的评论列表，按时间倒序排列
-    @Query("select a from Comment a where a.ownerID = ?1 order by a.cmtID DESC ")
+    //@Query("select a from Comment a where a.ownerID = ?1 order by a.cmtID DESC ")
+    @Query(value = "SELECT a.cmtid,a.commenterid,a.ownerid,a.text,a.time, b.nick_name AS commenter_name,b.avatar_url AS avatar_url  \n" +
+            "FROM comment a ,user b\n" +
+            "WHERE a.commenterid=b.user_id \n" +
+            "AND a.ownerid=?1 ORDER BY a.cmtid DESC",nativeQuery = true)
     List<Comment> queryByOwnerID(String userID);
 }
